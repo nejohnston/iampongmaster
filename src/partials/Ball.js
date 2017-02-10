9760e99c069f242c5c0ebce8118f5f2d8d974201
njohnstud <n.e.johnston@hotmail.com>
2017-02-10 11:21:39 -0800

    Added vectors for ball movement

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -14,10 +14,22 @@ export default class Ball {
 reset() {
   this.x = this.boardWidth / 2;
   this.y = this.boardHeight / 2;
+
+  this.vy = 0;
+
+  while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+  }
+
+
+  this.vx = this.direction * (6 - Math.abs(this.vy));
 }
 
   render(svg) {
-      let ball = document.createElementNS(SVG_NS, 'circle');
+    this.x += this.vx;
+    this.y += this.vy;
+
+    let ball = document.createElementNS(SVG_NS, 'circle');
     ball.setAttributeNS(null, 'r', 8);
     ball.setAttributeNS(null, 'cx', this.x);
     ball.setAttributeNS(null, 'cy', this.y);
